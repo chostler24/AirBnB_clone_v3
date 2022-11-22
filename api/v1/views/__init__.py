3913a9db6e45773d89974a475669fe4849d5e606
chostler24 <connor.hostler@gmail.com>
2022-11-22 16:02:39 -0600

    updates for task 4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
+#!/usr/bin/python3
+"""app_views init"""
+from flask import Blueprint
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
