3be6e66965a9ad6751ee684c8a630dbcc7ecac20
chostler24 <connor.hostler@gmail.com>
2022-11-28 10:18:13 -0600

    deleted states_2 and updated states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,60 +8,68 @@ from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 
 
-@app_views.route('/states/', methods=['GET'])
-def states_stop():
+@app_views.route('/states', methods=['GET'])
+def state_list():
+    """list all state objects"""
     return jsonify([
         state.to_dict() for state in storage.all(State).values()
     ])
 
+@app_views.route('/states/<state_id>', methods=['GET'])
+def state_get(state_id):
+    """retrieves state object"""
+    if state_id is not None:
+        return jsonify([
+            storage.get(State, state_id).to_dict()
+        ])
+    abort(404)
 
-@app_views.route(
-    '/states', methods=[
-        'GET', 'DELETE', 'POST', 'PUT'
-    ]
-)
-def states_slash(state_id=None):
-    """handles state routing if given specific state"""
-    req = flask.request.method
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def state_delete(state_id):
+    """deletes a state object"""
+    storage.delete(storage.get(State, state_id))
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'])
+def state_create():
+    """creates a State"""
+    if request.json():
+        json_req = request.json()
+        if "name" in json_req:
+            new = State(**json_req)
+            new.save()
+            return make_response(
+                jsonify(new.to_dict()), 201
+            )
+        return make_response(
+            jsonify({
+                "error": "Missing name"
+            }), 400
+        )
+    return make_response(
+        jsonify({
+            "error": "Not a JSON"
+        }), 400
+    )
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def state_update(state_id):
+    """updates state object"""
     loc = storage.get(State, state_id)
-    if loc is None:
-        abort(404)
-    if req == 'GET':
-        return jsonify(loc.to_dict())
-    if req == 'DELETE':
-        storage.delete(loc)
+    if request.get_json():
+        for k, v in request.get_json().items():
+            if k not in ("id", "created_at", "updated_at"):
+                loc[k] = v
         storage.save()
-        return make_response(jsonify({}), 200)
-    if req == 'POST':
-        if request.json():
-            json_req = request.json()
-            if "name" in json_req:
-                new = State(**json_req)
-                new.save()
-                return make_response(
-                    jsonify(new.to_dict()), 201
-                )
-            return make_response(
-                jsonify({
-                    "error": "Missing name"
-                }), 400
-            )
         return make_response(
-            jsonify({
-                "error": "Not a JSON"
-            }), 400
+            jsonify(loc.to_dict()), 400
         )
-    if req == 'PUT':
-        if request.get_json():
-            for k, v in request.get_json().items():
-                if k not in ("id", "created_at", "updated_at"):
-                    loc[k] = v
-            storage.save()
-            return make_response(
-                jsonify(loc.to_dict()), 400
-            )
-        return make_response(
-            jsonify({
-                "error": "Not a JSON"
-            }), 400
-        )
+    return make_response(
+        jsonify({
+            "error": "Not a JSON"
+        }), 400
+)
