cc95a4e55f5f6a15048b886247fc71bd66d7a3a4
conkobar <connorkleine@gmail.com>
2022-11-28 17:16:30 -0600

    task 12 maybe

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,35 +12,35 @@ from sqlalchemy.exc import IntegrityError
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'])
-def rev_list():
+def rev_list(place_id):
     """retrieves list of review objects"""
-    cit_dict = storage.all(City)
-    pl_list = None
-    for cit in cit_dict.values():
-        if cit.id == city_id:
-            pl_list = cit.places
-    if pl_list is not None:
+    pl_dict = storage.all(Place)
+    revs_list = None
+    for pl in pl_dict.values():
+        if pl.id == place_id:
+            revs_list = pl.reviews
+    if revs_list is not None:
         return jsonify(
-            [pl.to_dict() for pl in pl_list]
+            [rev.to_dict() for rev in revs_list]
         )
     abort(404)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'])
-def rev_get():
+def rev_get(review_id):
     """retrieves a review object"""
-    place_get = storage.get(Place, place_id)
-    if place_get is not None:
-        return jsonify(place_get.to_dict())
+    rev_get = storage.get(Review, review_id)
+    if rev_get is not None:
+        return jsonify(rev_get.to_dict())
     abort(404)
 
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE'])
-def rev_delete():
+def rev_delete(review_id):
     """deletes a review object"""
-    place_get = storage.get(Place, place_id)
-    if place_get is not None:
-        storage.delete(place_get)
+    rev_get = storage.get(Review, review_id)
+    if rev_get is not None:
+        storage.delete(rev_get)
         storage.save()
         return make_response(
             jsonify({}), 200
@@ -49,7 +49,7 @@ def rev_delete():
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['POST'])
-def rev_create():
+def rev_create(place_id):
     """creates a review object"""
     try:
         req = request.get_json()
@@ -58,15 +58,15 @@ def rev_create():
                 return make_response(jsonify(
                     {"error": "Missing user_id"}
                 ), 400)
-            if 'name' in req.keys():
-                req['city_id'] = city_id
-                new_place = Place(**req)
-                new_place.save()
+            if 'text' in req.keys():
+                req['place_id'] = place_id
+                new_rev = Review(**req)
+                new_rev.save()
                 return make_response(jsonify(
-                    new_place.to_dict()
+                    new_rev.to_dict()
                 ), 201)
             return make_response(jsonify(
-                {'error': 'Missing name'}
+                {'error': 'Missing text'}
             ), 400)
         return make_response(jsonify(
             {"error": "Not a JSON"}
@@ -76,19 +76,19 @@ def rev_create():
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'])
-def rev_update():
+def rev_update(review_id):
     """updates a review object"""
     req = request.get_json()
     if req is not None:
-        check_place = storage.get(Place, place_id)
-        if check_place is None:
+        rev_check = storage.get(Review, review_id)
+        if rev_check is None:
             abort(404)
         for key, val in req.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(check_place, key, val)
+                setattr(rev_check, key, val)
         storage.save()
         return make_response(jsonify(
-            check_place.to_dict()
+            rev_check.to_dict()
         ), 200)
     return make_response(jsonify(
         {"error": "Not a JSON"}
