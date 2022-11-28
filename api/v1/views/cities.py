2237dec5e75d02e81da06099d63058ff8fad4674
chostler24 <connor.hostler@gmail.com>
2022-11-28 15:13:50 -0600

    added final methods to cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,6 +7,7 @@ from models.state import State
 from models.city import City
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
+from sqlalchemy.exc import IntegrityError
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
@@ -48,10 +49,39 @@ def city_delete(city_id):
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
-def city_create():
+def city_create(state_id):
     """creates a city object"""
+    try:
+        req = request.get_json()
+        if req is not None:
+            if 'name' in req.keys() and req['name'] is not None:
+                req['state_id'] = state_id
+                new = City(**req)
+                new.save()
+                return make_response(jsonify(new.to_dict()), 201)
+            return make_response(jsonify({
+                'error': 'Missing name'
+                }), 400)
+        return make_response(jsonify({
+            "error": "Not a JSON"
+            }), 400)
+    except IntegrityError:
+        abort(404)
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
-def city_update():
+def city_update(city_id):
     """updates a city object"""
+    req = request.get_json()
+    if req is not None:
+        check_city = storage.get(City, city_id)
+        if check_city is None:
+            abort(404)
+        for key, val in req.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(check_city, key, val)
+        storage.save()
+        return make_response(jsonify(check_city.to_dict()), 200)
+    return make_response(jsonify({
+        "error": "Not a JSON"
+        }), 400)
